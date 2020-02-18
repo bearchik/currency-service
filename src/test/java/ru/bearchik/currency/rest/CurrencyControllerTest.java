34771aa5a0753b91e20b87e8cc6fe0c5c5ffc307
Malyshkin Mikhail <mikhail.malyshkin@evalar.com>
2020-02-18 11:48:56 +0300

    Fixed test

--- src/test/java/ru/bearchik/currency/rest/CurrencyControllerTest.java
+++ src/test/java/ru/bearchik/currency/rest/CurrencyControllerTest.java
@@ -11,7 +11,7 @@ class CurrencyControllerTest extends Assert {
 
     @Test
     void index() {
-        assertEquals(currencyController.index(), "passed");
+        assertEquals(currencyController.index(), "Test passed. App v1");
     }
 
 }
